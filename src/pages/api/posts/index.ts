891f05468f44e1caaca3b2e5909937e50fb90ff4
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-09-12 17:07:02 -0300

    106. SSR Adapters

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,6 +1,10 @@
 import type  { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
 
+//esta ruta no se genera estaticamente
+//preciso que sea generada cuando se haga una solicitud
+export const prerender = false;
+
 export const GET: APIRoute = async ({ params, request }) => {
 
     const posts = await getCollection('blog');
