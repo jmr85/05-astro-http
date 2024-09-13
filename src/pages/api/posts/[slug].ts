6e6448bfc1b257970120852ea039ceb6ff3ced07
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-09-13 20:55:36 -0300

    110. Post, Put, Delete Endpoints

--- src/pages/api/posts/[slug].ts
+++ src/pages/api/posts/[slug].ts
@@ -28,6 +28,69 @@ export const GET: APIRoute = async ({ params, request }) => {
     });
 };
 
+
+export const POST: APIRoute = async ({ params, request }) => {
+
+    const { body } = await request.json();
+    
+    return new Response(JSON.stringify({
+        method: 'POST',
+        ...body,
+    }), { 
+        status: 200,
+        headers: { 
+            'Content-Type': 'application/json' 
+        } 
+    });
+};
+
+export const PUT: APIRoute = async ({ params, request }) => {
+
+    const { body } = await request.json();
+    
+    return new Response(JSON.stringify({
+        method: 'PUT',
+        ...body,
+    }), { 
+        status: 200,
+        headers: { 
+            'Content-Type': 'application/json' 
+        } 
+    });
+};
+
+export const PATCH: APIRoute = async ({ params, request }) => {
+
+    const { body } = await request.json();
+    
+    return new Response(JSON.stringify({
+        method: 'PATCH',
+        ...body,
+    }), { 
+        status: 200,
+        headers: { 
+            'Content-Type': 'application/json' 
+        } 
+    });
+};
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+
+    const { slug } = params;
+    
+    return new Response(JSON.stringify({
+        method: 'DELETE',
+        slug: slug,
+    }), { 
+        status: 200,
+        headers: { 
+            'Content-Type': 'application/json' 
+        } 
+    });
+};
+
+
+
 // export const getStaticPaths: GetStaticPaths = async () => {
 //     return [
 //         {
