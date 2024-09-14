5679e2e7684bc5053b246aea3aa31ab307e377e6
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-09-13 23:48:10 -0300

    111. Despliegue a Cloudflare - add cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,15 +4,20 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
-import node from '@astrojs/node';
+import cloudflare from '@astrojs/cloudflare';
+
+//import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap()],
-  output: 'hybrid',
 
-  adapter: node({
-    mode: 'standalone',
-  }),
+  //output: 'hybrid',
+  // adapter: node({
+  //   mode: 'standalone',
+  // }),
+  integrations: [mdx(), sitemap()],
+
+  output: 'server',
+  adapter: cloudflare(),
 });
