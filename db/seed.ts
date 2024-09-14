81b78e43f7c1ae8a4dd88ab61fe561f29433a501
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-09-14 19:19:25 -0300

    117. Crear tabla de clientes

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,13 @@
-import { db } from 'astro:db';
+import { Clients, db } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
+	await db.insert(Clients).values([
+		{ name: 'Kasim', age: 35, isActive: true },
+		{ name: 'Fernando', age: 25, isActive: true },
+		{ name: 'Carlos', age: 37, isActive: true },
+		{ name: 'Melissa', age: 25, isActive: false },
+		{ name: 'Emin', age: 15, isActive: true },
+	]);
 	console.log('Seed executed');
 }
