3a6df9b98ec2a119fed86760d7c067c9fd5343d3
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-09-15 05:31:34 -0300

    119. CRUD con Astro DB - Post y Get

--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,4 +1,5 @@
 import type  { APIRoute } from 'astro';
+import { Clients, db } from 'astro:db';
 
 //esta ruta no se genera estaticamente
 //preciso que sea generada cuando se haga una solicitud
@@ -6,11 +7,9 @@ export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
 
-    const body = {
-        method: 'GET'
-    }
+    const clients = await db.select().from(Clients);
 
-    return new Response(JSON.stringify(body), { 
+    return new Response(JSON.stringify(clients), { 
         status: 200,
             headers: { 
             'Content-Type': 'application/json' 
@@ -20,14 +19,28 @@ export const GET: APIRoute = async ({ params, request }) => {
 
 export const POST: APIRoute = async ({ params, request }) => {
 
-    const body = {
-        method: 'POST',
-    }
+    try{
+        const { id, ...body } = await request.json();
 
-    return new Response(JSON.stringify(body), { 
-        status: 201,
-            headers: { 
-            'Content-Type': 'application/json' 
-        } 
-    });
+        const { lastInsertRowid } = await db.insert(Clients).values(body);
+
+        return new Response(JSON.stringify({
+            id: +lastInsertRowid!.toString(),
+            ...body,
+        }), { 
+            status: 201,
+                headers: { 
+                'Content-Type': 'application/json' 
+            } 
+        });
+    }catch (error){
+        console.log(error);
+
+        return new Response(JSON.stringify({ msg: 'No body found' }), {
+          status: 201,
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        });
+    }
 };
