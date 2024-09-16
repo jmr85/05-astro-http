967fe039b28865afbdc5fcc5583e74f0c95ac1b2
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-09-16 16:43:35 -0300

    130. Put - Incrementar la cantidad de likes- API Rest

--- src/pages/api/likes/[id].ts
+++ src/pages/api/likes/[id].ts
@@ -31,3 +31,29 @@ export const GET: APIRoute = async ({ params }) => {
         },
     });
 }
+
+export const PUT: APIRoute = async ({ params, request }) => {
+
+    const postId = params.id ?? '';
+
+    const posts = await db.select().from(Posts).where(eq(Posts.id, postId));// busca el post
+    const { likes = 0 } = await request.json();// Si no viene nada a va ser 0
+
+    if (posts.length === 0) {
+        const newPost = {
+            id: postId,
+            title: 'Post not found',
+            likes: 0,
+        };
+
+        await db.insert(Posts).values(newPost);
+        posts.push(newPost);//agrega el nuevo post
+    }
+
+    const post = posts.at(0)!;//obtiene el primer post
+    post.likes = post.likes + likes;//actualiza la cantidad de likes
+
+    await db.update(Posts).set(post).where(eq(Posts.id, postId));
+
+    return new Response('OK!', {status: 200} );
+}
