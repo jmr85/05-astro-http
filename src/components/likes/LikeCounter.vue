4336aa7de6c2c6b2c8ba8d55137fddc63a42a8d4
Juan Martin Ruiz <juan.martin.ruiz@gmail.com>
2024-09-17 11:49:19 -0300

    132. Vue - Props y cantidad de likes

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -1,13 +1,48 @@
 <template>
-    <button @click="likePost">
-        Like Counter
+    <div v-if="isLoading">
+        Loading...
+    </div>
+    <button v-else-if="likeCount === 0" @click="likePost">
+        Like this post
+    </button>
+    <button v-else @click="likePost">
+        Likes
+        <span>{{ likeCount }}</span>
     </button>
 </template>
 
 <script lang="ts" setup>
+    import { ref, defineProps } from 'vue';
+
+    interface Props {
+        postId: string;
+    }
+
+    const props = defineProps<Props>();
+
+    console.log(props.postId);
+
+    const likeCount = ref(0);
+    const likeClicks = ref(0);//debaunce para no mandar para cada click
+    const isLoading = ref(true);//se puede hacer con TanStack Query
+
     const likePost = () => {
         console.log('+1 like post');
     }
+
+    const getCurrentLikes = async () => {
+        const resp = await fetch(`/api/posts/likes/${ props.postId }`);
+        if ( !resp.ok ) return;
+
+        const data = await resp.json();
+
+        console.log('data', data);
+        
+        likeCount.value = data.likes;
+        isLoading.value = false; 
+    }
+
+    getCurrentLikes();
 </script>
 
 <style scoped>
